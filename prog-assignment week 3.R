500a747b54527f815b58de932116e76e48bb955c
AHRAR-HASHMI <55481336+AHRAR-HASHMI@users.noreply.github.com>
2020-04-28 12:10:25 +0530

    Add files via upload

---
+++ prog-assignment week 3.R
@@ -0,0 +1,32 @@
+makeCacheMatrix <- function(x = matrix()) {
+    inverse <- NULL
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function(){
+        x 
+    } 
+    setinverse <- function(inverse) {
+        inverse <<- inverse 
+    }
+    getinverse <- function() {
+        inverse 
+    }
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
+    
+}
+
+cacheSolve <- function(x, ...) {
+    inv <- x$getinverse()
+    if(!is.null(inv)) {
+        print("getting data")
+        return(inv)
+    }
+    data <- x$get()
+    inv <- solve(data, ...)
+    x$setinverse(inv)
+    inv
+}
